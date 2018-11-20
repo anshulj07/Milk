6c1c8e36d321284cef77b19b56a6562066902d81
Gaurav Gupta <gaurav.gupta@snapwiz.com>
2018-11-21 00:27:12 +0530

    Added increment decrement functionality

--- js/main.js
+++ js/main.js
@@ -1,36 +1,20 @@
 jQuery(document).ready(function () {
     // This button will increment the value
-    $('.increase').click(function (e) {
-        // Stop acting like a button
-        e.preventDefault();
-        // Get the field name
-        fieldName = $(this).attr('field');
-        // Get its current value
-        var currentVal = parseInt($('input[name=' + fieldName + ']').val());
-        // If is not undefined
-        if (!isNaN(currentVal)) {
-            // Increment
-            $('input[name=' + fieldName + ']').val(currentVal + 1);
-        } else {
-            // Otherwise put a 0 there
-            $('input[name=' + fieldName + ']').val(0);
-        }
+    $('.increase').click(function (ev) {
+        var currentTarget = $(ev.currentTarget);
+        var inputElement = currentTarget.prev('.number');
+        var inputValue = parseInt(inputElement.val());
+        inputElement.val(inputValue + 1);
+        currentTarget.closest('.myform').find('.addtocart').attr('data-value', inputValue + 1);
     });
     // This button will decrement the value till 0
-    $(".decrease").click(function (e) {
-        // Stop acting like a button
-        e.preventDefault();
-        // Get the field name
-        fieldName = $(this).attr('field');
-        // Get its current value
-        var currentVal = parseInt($('input[name=' + fieldName + ']').val());
-        // If it isn't undefined or its greater than 0
-        if (!isNaN(currentVal) && currentVal > 0) {
-            // Decrement one
-            $('input[name=' + fieldName + ']').val(currentVal - 1);
-        } else {
-            // Otherwise put a 0 there
-            $('input[name=' + fieldName + ']').val(0);
+    $(".decrease").click(function (ev) {
+        var currentTarget = $(ev.currentTarget);
+        var inputElement = currentTarget.next('.number');
+        var inputValue = parseInt(inputElement.val());
+        if (inputValue !== 0) {
+            inputElement.val(inputValue - 1);
+            currentTarget.closest('.myform').find('.addtocart').attr('data-value', inputValue - 1);
         }
     });
 });
